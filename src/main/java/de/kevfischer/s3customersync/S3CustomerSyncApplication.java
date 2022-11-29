8335854d0366c35ea14a5b4aa4d30f697d2c25bc
Kevin Fischer <kfischer@united-internet.de>
2022-11-29 15:20:33 +0100

    Initial commit

---
+++ src/main/java/de/kevfischer/s3customersync/S3CustomerSyncApplication.java
@@ -0,0 +1,13 @@
+package de.kevfischer.s3customersync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3CustomerSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3CustomerSyncApplication.class, args);
+	}
+
+}
